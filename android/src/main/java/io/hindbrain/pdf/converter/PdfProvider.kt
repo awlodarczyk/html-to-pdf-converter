34ac7e31b273d0ad677905147d820301446e7d19
Adam Włodarczyk <adam.wlodarczyk@coderslab.pl>
2024-06-17 11:26:05 +0200

    commit

---
+++ android/src/main/java/io/hindbrain/pdf/converter/PdfProvider.kt
@@ -0,0 +1,9 @@
+package io.hindbrain.pdf.converter
+import androidx.core.content.FileProvider
+
+/**
+ * We need this class for android.support.v4.content.FileProvider
+ * Since Android Nougat we need this to be declared in AndroidManifest.xml
+ * in order to open PDF file in another application
+ */
+class PdfProvider : FileProvider()
--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,21 @@ import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-cor
 // Import the native module. On web, it will be resolved to HtmlToPdfConverter.web.ts
 // and on native platforms to HtmlToPdfConverter.ts
 import HtmlToPdfConverterModule from './HtmlToPdfConverterModule';
-import HtmlToPdfConverterView from './HtmlToPdfConverterView';
-import { ChangeEventPayload, HtmlToPdfConverterViewProps } from './HtmlToPdfConverter.types';
+import { ChangeEventPayload } from './HtmlToPdfConverter.types';
 
-// Get the native constant value.
-export const PI = HtmlToPdfConverterModule.PI;
 
-export function hello(): string {
-  return HtmlToPdfConverterModule.hello();
-}
 
-export async function setValueAsync(value: string) {
-  return await HtmlToPdfConverterModule.setValueAsync(value);
+export async function convert(value: string) {
+  return await HtmlToPdfConverterModule.convert(value);
 }
 
 const emitter = new EventEmitter(HtmlToPdfConverterModule ?? NativeModulesProxy.HtmlToPdfConverter);
 
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+export function addOnPdfCreatedListener(listener: (event: ChangeEventPayload) => void): Subscription {
+  return emitter.addListener<ChangeEventPayload>('onPdfCreated', listener);
+}
+export function addOnPdfFailedListener(listener: (event: ChangeEventPayload) => void): Subscription {
+  return emitter.addListener<ChangeEventPayload>('onPdfFailed', listener);
 }
 
-export { HtmlToPdfConverterView, HtmlToPdfConverterViewProps, ChangeEventPayload };
+export { ChangeEventPayload };
